02da860ac8ea18f686e812fa3423148d8cc55a92
Níz Peña <nizadiel5@gmail.com>
2025-06-23 11:19:39 -0700

    RQ:01: Movimiento basico agregado

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,33 +1,20 @@
-# player_imperative_modular.gd
 extends CharacterBody2D
 
-var speed := 200
-var run := 500
-var is_runing := false
+var velocidad = 200
+var brinco = -400
+var gravedad = 1000
 
-func _physics_process(delta: float) -> void:
-	handle_movement()
-	handle_dash()
-	apply_velocity()
+func _ready():
+	add_to_group("jugador")
 
-func handle_movement() -> void:
-	var direction := Vector2.ZERO
-	if Input.is_action_pressed("ui_d"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_a"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_s"):
-		pass
-	if Input.is_action_pressed("ui_w"):
-		pass
-	velocity = direction.normalized()
-
-func handle_dash() -> void:
-	if Input.is_action_just_pressed("ui_shift"):
-		is_runing = true
-	elif Input.is_action_just_released("ui_shift"):
-		is_runing = false
-
-func apply_velocity() -> void:
-	velocity *= run if is_runing else speed
+func _physics_process(delta):
+	var direccion = Input.get_axis("ui_left","ui_right")
+	velocity.x = direccion * velocidad
+	
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = brinco
+	
 	move_and_slide()
